19d5bc6061cd7107f2a2e5b01fcd174aec9349e2
Rodrigo Lopes <83999059+rodrigofblopes@users.noreply.github.com>
2025-07-06 22:11:52 -0400

    Add files via upload
    Signed-off-by: Rodrigo Lopes <83999059+rodrigofblopes@users.noreply.github.com>

--- server.py
+++ server.py
@@ -8,4 +8,16 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Servidor rodando em http://localhost:{PORT}")
     webbrowser.open(f"http://localhost:{PORT}")
+=======
+import http.server
+import socketserver
+import webbrowser
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Servidor rodando em http://localhost:{PORT}")
+    webbrowser.open(f"http://localhost:{PORT}")
+>>>>>>> d2ba42b (Primeiro commit do projeto ViewPro 3D)
     httpd.serve_forever()
